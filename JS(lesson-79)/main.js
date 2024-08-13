c33b96e16b2540c916bd797394b5cc8e3c5f7118
generalovapolina <145095797+generalovapolina@users.noreply.github.com>
2024-08-13 16:49:27 +0300

    Add files via upload

---
+++ JS(lesson-79)/main.js
@@ -0,0 +1,22 @@
+const firstAge = 23
+const secondAge = 18
+
+if (firstAge>=secondAge) {
+const result = "Вы совершеннолетний"
+console.log(result)
+} 
+else {
+const result = "Вам еще нет 18"
+console.log(result)
+}
+
+// ================================================
+
+
+const age = 23
+
+let acessAllowed = (age>=18)? 'Вы совершеннолетний!' : 'Вам еще нет 18';
+
+console.log(acessAllowed)
+
+
